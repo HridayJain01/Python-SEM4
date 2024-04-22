3cd848a9204836ead351301316d3280ff6382603
shreyachawale <shreyachawale08@gmail.com>
2024-04-22 22:09:25 +0530

    Create Q47.py

---
+++ prac_questions/Q47.py
@@ -0,0 +1,17 @@
+# get maximum and minimum values of didctionary
+
+sample_dict = {
+    "a": 10,
+    "b": 20,
+    "c": 5,
+    "d": 37,
+    "e": 40
+}
+
+max_value = max(sample_dict.values())
+
+min_value = min(sample_dict.values())
+
+print("Dictionary:", sample_dict)
+print("Maximum value in dictionary:", max_value)
+print("Minimum value in the dictionary:", min_value)
